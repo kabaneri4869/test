0cb63aaeadc2259f9441210b369cf0b8edef3b1e
kabaneri4869 <120975536+kabaneri4869@users.noreply.github.com>
2022-12-21 19:09:44 +0530

    Delete 1.py

--- 1.py
+++
@@ -1,8 +0,0 @@
-n=int(input("Enter the code word: "))
-print("The code word entered by the user is: ",n)
-parity=0
-while(n!=0):
-	if((n&1)==1):
-		parity^=1
-		n>>=1
-		print("Parity of the number is: ",parity)
